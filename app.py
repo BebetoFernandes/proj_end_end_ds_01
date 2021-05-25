efda1730e86a10e3f160ffdf5dce10b7f02d484e
Alberto Leonardo de Lima Fernandes <re041214@resource.com.br>
2021-05-25 16:34:46 -0300

    Primeiro versão App

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.header("Olá Mundo!")
+st.title("Calculadora de IMC")
+st.text("Informe sua altura e seu peso para saber o seu peso! (Em metros e kg)")
+st.markdown("---")
+
+peso = float(st.text_input("Qual é o seu peso?", 80))
+altura = float(st.text_input("Qual é a sua altura?", 1.6))
+
+botao = st.button("Calcular IMC")
+st.markdown("---")
+
+def IMC(peso,altura):
+    return round((peso / altura **2),2)
+
+if botao == True:
+    st.subheader("Abaixo estão os seus resultados:")
+    valor = IMC(peso,altura)
+    if valor <= 18.5:
+        st.markdown(f"O seu IMC é de {valor}, você está abaixo do seu peso ideal! :pensive:")
+    elif valor >= 25:
+        st.markdown(f"O seu IMC é de {valor}, você está acima do seu peso ideal! :dizzy_face:")
+    else:
+        st.markdown(f"O seu IMC é de {valor}, você está dentro do seu peso ideal! :grin:")
+
+
